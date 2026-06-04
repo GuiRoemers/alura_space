8b0dd8970a90516ac90c472f28caa4cdd65d2415
guilhermerm-weg <guilhermerm@weg.net>
2026-06-04 07:58:16 -0300

    Projeto finalizado aula 2

--- galeria/urls.py
+++ galeria/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('mais_vistas/', mais_vistas, name='mais_vistas'),
     path('novas/', novas, name='novas'),
     path('surpreenda_me/', surpreenda_me, name='surpreenda_me'),
+    path('buscar/', buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -21,3 +21,13 @@ def novas(request):
 
 def surpreenda_me(request):
     return render(request, 'galeria/surpreenda_me.html')
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
